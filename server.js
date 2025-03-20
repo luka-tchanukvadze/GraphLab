7917083b7cd48de77d45284cd11282e4161e9c8f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-20 19:09:12 +0400

    setting up the basic graphql-yoga server

--- server.js
+++ server.js
@@ -1,101 +1,25 @@
-var {
-  graphql,
-  buildSchema,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-} = require("graphql");
-const express = require("express");
-const { createHandler } = require("graphql-http/lib/use/express");
-const { ruruHTML } = require("ruru/server");
+import express from "express";
+import { createSchema, createYoga } from "graphql-yoga";
+import { ruruHTML } from "ruru/server";
 
-// var schema = buildSchema(`
-//   type Query {
-//     hello(name: String!): String
-
-//     age: Int
-//     weight: Float!
-//     isOver18: Boolean
-//     hobbies: [String!]!
-
-//     user: User
-//   }
-
-//   type User {
-//     id: Int
-//     name: String
-
-//   }
-
-// `);
-
-// var rootValue = {
-//   hello: ({ name }) => {
-//     return `hello ${name}`;
-//   },
-
-//   age: () => {
-//     return 3;
-//   },
-
-//   weight: 55,
-//   isOver18: true,
-//   hobbies: () => {
-//     return ["carting", "f1", "random "];
-//   },
-
-//   user: () => {
-//     return {
-//       id: 1,
-//       name: "Chanu",
-//     };
-//   },
-// };
-
-// const User = new GraphQLObjectType({
-//   name: "User",
-//   fields: {
-//     id: {
-//       type: GraphQLInt,
-//     },
-//     name: {
-//       type: GraphQLString,
-//       resolve: (obj) => {
-//         return obj.name.trim();
-//       },
-//     },
-
-//   },
-// });
-
-// const schema = new GraphQLSchema({
-//   query: new GraphQLObjectType({
-//     name: "Query",
-//     fields: {
-//       hello: {
-//         type: GraphQLString,
-//         resolve: () => {
-//           return "Hello world";
-//         },
-//       },
-
-//       user: {
-//         type: User,
-//         resolve: () => {
-//           return {
-//             id: 1,
-//             name: "Chanu",
-//           };
-//         },
-//       },
-//     },
-//   }),
-// });
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello from yoga",
+      },
+    },
+  }),
+});
 
 const app = express();
 
-app.all("/graphql", createHandler({ schema }));
+app.all("/graphql", yoga);
 
 app.get("/", (_req, res) => {
   res.type("html");
