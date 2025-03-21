28758522f987c57d926afdc1446a117c0d61f383
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-21 23:50:04 +0400

    working on user

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -4,11 +4,29 @@ export const schema = createSchema({
   typeDefs: /* GraphQL */ `
     type Query {
       hello: String
+      user: User
+    }
+
+    type User {
+      id: Int
+      name: String
     }
   `,
   resolvers: {
     Query: {
       hello: () => "Hello from yoga",
+      user: () => {
+        return {
+          id: 1,
+          name: "Luka",
+        };
+      },
+    },
+
+    User: {
+      name: (obj) => {
+        return obj.name.toUpperCase();
+      },
     },
   },
 });
