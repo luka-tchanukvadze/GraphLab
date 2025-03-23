2bcfa4aa5a0f5b068a81e77d0d0011ebcb674a31
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-23 17:01:29 +0400

    usyn sample_mflix for testing connection

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -18,10 +18,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const movies = await client
+      .db("sample_mflix")
+      .collection("movies")
+      .find()
+      .toArray();
+    console.log(movies);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
